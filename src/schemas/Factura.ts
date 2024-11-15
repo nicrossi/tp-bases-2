c1c5a074281ed43702f1558afd421163e12d2270
dlimachi <dlimachi@itba.edu.ar>
2024-11-14 23:48:02 -0300

    Add factura

---
+++ src/schemas/Factura.ts
@@ -0,0 +1,39 @@
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface Invoice extends Document {
+    nro_factura: number;
+    fecha: Date;
+    total_sin_iva: number;
+    iva: number;
+    total_con_iva: number;
+    nro_cliente: number
+    items: [{
+        nro_item: number;
+        cantidad: number;
+        codigo_producto: string;
+    }];
+}
+
+const FacturaSchema = new Schema<Invoice>({
+    nro_factura: { type: Number, required: true, unique: true },
+    fecha: { type: Date, required: true },
+    total_sin_iva: { type: Number, required: true },
+    iva: { type: Number, required: true },
+    total_con_iva: { type: Number, required: true },
+    nro_cliente: { type: Number, required: true }, 
+    items: [
+        {
+            nro_item: { type: Number, required: true },
+            cantidad: { type: Number, required: true },
+            codigo_producto: { type: String, required: true }
+        }
+    ]
+});
+
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+FacturaSchema.plugin(AutoIncrement, { inc_field: 'nro_factura' });
+
+const Factura = mongoose.model<Invoice>('Factura', FacturaSchema);
+
+export default Factura;
+ 
